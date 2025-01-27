58b81e1110c6725e502fc6342dbfff4aab2037e5
Janviere-dev <j.munezero@alustudent.com>
2025-01-27 23:59:32 +0200

    added names to cohort-2.txt

---
+++ the_coder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#the following line is for creating directory called " students_ directory"
+mkdir students_directory
+# create file inside students_directory
+touch students_directory/cohort-1.txt
+touch students_directory/cohort-2.txt
+touch students_directory/cohort-3.txt
+
+# adding names to "cohort-2.txt"
+echo  -e "Janviere\nGabriella\nMonica\nBuke\nKelly\n "> cohort-2.txt
