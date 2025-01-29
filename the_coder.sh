05996cfeecf14fc5417d6dcadd048a2c20185920
garangbse <g.buke@alustudent.com>
2025-01-29 02:15:42 -0800

    Updated changes up to part 4

--- the_coder.sh
+++ the_coder.sh
@@ -7,7 +7,7 @@ touch students_directory/cohort-2.txt
 touch students_directory/cohort-3.txt
 
 # adding names to "cohort-2.txt"
-echo  -e "Janviere\nGabriella\nMonica\nBuke\nKelly\n "> cohort-2.txt
+echo  -e "Janviere\nGabriella\nMonica\n "> cohort-2.txt
 # Update to student names in cohort-1.txt and cohort-3.txt
 echo  -e "Winston\nChurchill\nBruce\n "> cohort-1.txt
 echo  -e "Nzengele\nSolim\nLuyando\n "> cohort-3.txt
